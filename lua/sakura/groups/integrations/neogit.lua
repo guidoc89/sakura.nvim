0c49df51076852955b1fc9f50092cc30b73f618f
guido <guid.castr@gmail.com>
2025-02-04 02:06:32 +0100

    feat(neogit): add groups for diffs hovering

--- lua/sakura/groups/integrations/neogit.lua
+++ lua/sakura/groups/integrations/neogit.lua
@@ -4,7 +4,13 @@ return {
 	NeogitDiffDeleteHighlight = { bg = palette.er9, fg = palette.er0 },
 	-- Only show bg on current diff focus
 	NeogitDiffDelete = { bg = nil, fg = palette.er0 },
-	NeogitDiffAdd = { bg = nil, fg = palette.gr0 },
+	NeogitDiffDeleteCursor = { bg = palette.er0, fg = palette.er9 },
 	NeogitChangeDeleted = { fg = palette.er0 },
+
+	-- Only show bg on current diff focus
+	NeogitDiffAdd = { bg = nil, fg = palette.gr0 },
+	NeogitDiffAddCursor = { bg = palette.gr0, fg = palette.gr9 },
+
 	NeogitChangeModified = { fg = palette.yl0 },
+	NeogitDiffContextCursor = { bg = palette.pi2 },
 }
