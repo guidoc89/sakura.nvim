c483892430376ffb9cf86f0ed1c07fc2a9a595e5
guido <guid.castr@gmail.com>
2025-02-08 17:38:49 +0100

    feat(todo-comments): add fg hl for NOTE and TODO

--- lua/sakura/groups/integrations/todo_comments.lua
+++ lua/sakura/groups/integrations/todo_comments.lua
@@ -2,10 +2,12 @@ local palette = require("sakura.palette")
 
 return {
 	TodoBgTODO = { bg = palette.sa0, fg = palette.bgdefault, bold = true },
+	TodoFgTODO = { fg = palette.sa0, bold = true },
 	TodoBgNOTE = { bg = palette.sa1, fg = palette.bgdefault, bold = true },
-	TodoBgPERF = { bg = palette. sr0, fg = palette.bgdefault, bold = true },
+	TodoFgNOTE = { fg = palette.sa1, bold = true },
+	TodoBgPERF = { bg = palette.sr0, fg = palette.bgdefault, bold = true },
 	TodoBgHACK = { bg = palette.yl0, fg = palette.bgdefault, bold = true },
-	TodoBgWARN = { bg = palette.yl0,  fg = palette.bgdefault, bold = true },
+	TodoBgWARN = { bg = palette.yl0, fg = palette.bgdefault, bold = true },
 	TodoBgFIX = { bg = palette.er0, fg = palette.bgdefault, bold = true },
 	TodoBgBUG = { bg = palette.er0, fg = palette.bgdefault, bold = true },
 }
